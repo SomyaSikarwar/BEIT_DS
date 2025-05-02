89e593157ae912c11560fb7692788b77a91e14fc
Sunandan Phalke <sunandan.phalke@elastic.run>
2025-05-02 22:20:30 +0530

    Organized

--- Assignment_6/BullyAlgorithm.java
+++ Assignment_6/BullyAlgorithm.java
@@ -1,3 +1,5 @@
+package Assignment_6;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
--- Assignment_6/RingAlgorithm.java
+++ Assignment_6/RingAlgorithm.java
@@ -1,3 +1,5 @@
+package Assignment_6;
+
 import java.util.*;
 
 public class RingAlgorithm {
