08dca1ae0fd998827a6790243149caebc852a3c6
SomyaSikarwar <118437254+SomyaSikarwar@users.noreply.github.com>
2025-05-09 10:46:02 +0530

    Updated RingAlgorithm

--- Assignment_6/RingAlgorithm.java
+++ Assignment_6/RingAlgorithm.java
@@ -4,49 +4,53 @@ public class RingAlgorithm {
 
     public static void election(int nodes, int initiator, int[] isActive) {
         int[] arr = new int[nodes];
-        int count = 1;
-        arr[0] = initiator + 1;
-        int current = (initiator + 1) % nodes;
+        int count = 0;
+        int current = initiator;
 
         do {
             if (isActive[current] != 0) {
                 arr[count] = current + 1;
-                System.out.println("Process " + (initiator + 1) + " sends the Election message to process " + (current + 1));
+                int next = (current + 1) % nodes;
+                while (isActive[next] == 0) {
+                    next = (next + 1) % nodes;
+                }
+                System.out.println("Process " + (current + 1) + " sends the Election message to process " + (next + 1));
                 count++;
                 printArr(arr, count);
+                current = next;
+            } else {
+                current = (current + 1) % nodes;
             }
-            current = (current + 1) % nodes;
         } while (current != initiator);
 
-        int Max = newLeader(arr,count);
+        int Max = newLeader(arr, count);
         System.out.println("Process " + Max + " is the new elected leader!!!");
     }
 
-
-    public static void printArr(int[] arr , int count){
+    public static void printArr(int[] arr, int count) {
         System.out.print("[ ");
-        for(int i = 0 ; i < count ; i++){
+        for (int i = 0; i < count; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println("]");
     }
 
-    public static int newLeader(int[] arr, int count){
+    public static int newLeader(int[] arr, int count) {
         int Max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < count ; i++){
-            Max = Math.max(Max , arr[i]);
+        for (int i = 0; i < count; i++) {
+            Max = Math.max(Max, arr[i]);
         }
         return Max;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of nodes :");
         int nodes = sc.nextInt();
         int[] isActive = new int[nodes];
         int failed_leader = nodes;
 
-        for(int i = 0 ; i < nodes ; i++){
+        for (int i = 0; i < nodes; i++) {
             isActive[i] = 1;
         }
 
@@ -55,6 +59,6 @@ public class RingAlgorithm {
         int initiator = sc.nextInt() - 1;
 
         System.out.println("Process " + failed_leader + " failed as a leader");
-        election(nodes , initiator , isActive);
+        election(nodes, initiator, isActive);
     }
 }
