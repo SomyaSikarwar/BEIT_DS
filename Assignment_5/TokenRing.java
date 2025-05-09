32106d5183754d8a7bcc25b93cb13c7a1f868c8f
Sunandan Phalke <77063671+SunandanP@users.noreply.github.com>
2025-05-09 18:43:27 +0000

    Optimized Code

--- Assignment_5/TokenRing.java
+++ Assignment_5/TokenRing.java
@@ -1,58 +1,68 @@
 import java.util.*;
 
 public class TokenRing {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter number of processes: ");
-        int n = sc.nextInt();
-
-        System.out.println("The processes are:");
-        for (int i = 1; i <= n; i++) {
-            System.out.print(i + " ");
-        }
-
-        int choice = 1;
-        int token = 1;
-        sc.nextLine(); // Consume leftover newline
-
-        while (choice == 1) {
-            System.out.println("\nEnter the message: ");
-            String message = sc.nextLine();
-
-            System.out.println("Enter the sender: ");
-            int sender = sc.nextInt();
-
-            System.out.println("Enter the receiver: ");
-            int receiver = sc.nextInt();
-
-            System.out.print("Token Passing: ");
-            int i = token;
-            while (i != sender) {
-                System.out.print(i + " -> ");
-                i = (i % n) + 1;
-            }
-            System.out.println(sender);
-
-            System.out.println("Sender " + sender + " sending message: " + message);
-
-            i = (sender % n) + 1;
-            while (i != receiver) {
-                System.out.println("Message forwarded by process " + i);
-                i = (i % n) + 1;
-            }
-
-            System.out.println("Receiver " + receiver + " received the message: " + message);
-            System.out.println("Acknowledgement sent back to sender: " + sender);
-
-            token = sender;
-
-            System.out.println("Press 1 to continue or 0 to exit: ");
-            choice = sc.nextInt();
-            sc.nextLine(); // Consume newline after nextInt()
-        }
-
-        System.out.println("Program Ended.");
+  public static void main(String[] args){
+    List<Integer> processes = new ArrayList<>();
+    
+    System.out.println("Enter the number of processes");
+    Scanner sc = new Scanner(System.in);
+    
+    int numberOfProcesses = sc.nextInt();
+    for(int i = 1; i <= numberOfProcesses; i++){
+      processes.add(i);
     }
+    
+    System.out.println("Processes are : " + processes);
+    
+    int count = 0;
+    while(true){
+      if(count != 0){
+        System.out.println("Do you want to continue? Enter 1 for YES, 0 for NO");
+        int input = sc.nextInt();
+        if(input == 0) break;
+      }
+    
+      System.out.println("Enter the number of sender process : ");
+      int sender = sc.nextInt();
+      sc.nextLine();
+      System.out.println("Enter the message to be sent : ");
+      String message = sc.nextLine();
+      System.out.println("Enter the number of receiver process : ");
+      int receiver = sc.nextInt();
+  
+      simulateTokenRing(sender, receiver, message, processes);
+      count++;
+      
+    }
+  }
+  
+  public static void simulateTokenRing(int sender, int receiver, String message, List<Integer> processes){
+    int numberOfProcesses = processes.size();
+    boolean round = false;
+    for(int current = 0; current < processes.size(); current = (current+1) % numberOfProcesses){
+      int currentProcess = processes.get(current);
+      if(sender > receiver && currentProcess >= sender) round = true;
+      if(currentProcess == receiver){
+        if(sender > receiver){
+          if(round){
+            System.out.println("Message delivered to the receiver process : " + currentProcess + "\nMessage : "+ message);
+            receiver = -1;
+            message = "Acknowledgment";
+          }
+        } else {
+          System.out.println("Message delivered to the receiver process : " + currentProcess + "\nMessage : "+ message);
+          receiver = -1;
+          message = "Acknowledgment";
+        }
+        
+      }
+      else if(currentProcess == sender && receiver == -1){
+        System.out.println(message + " delivered to the sender process : " + currentProcess);
+        break;
+      }
+      else {
+        System.out.println("Message forwarded by process " + currentProcess);
+      }
+    }
+  }
 }
